6d033dffbd111e7e941587741f950e27b890d415
Ron Duwell <ronduwell@zipcoders-MacBook-Pro-4.local>
2019-11-18 10:46:46 -0500

    gott go to class

---
+++ src/main/java/services/ComicServices.java
@@ -0,0 +1,12 @@
+package services;
+
+import models.Comic;
+
+import java.util.ArrayList;
+
+public class ComicServices {
+
+    public static Integer nextId = 1;
+
+    private ArrayList <Comic> inventory = new ArrayList<>();
+}
